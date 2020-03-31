7189b8efe43ebb40d02e8bb9f5b1dc55f02a18ad
sona923 <61297011+sona923@users.noreply.github.com>
2020-03-31 10:25:52 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com  ");
-        System.out.println("Sona Yadav");
+        System.out.print("Sona Yadav");
     }
 }
